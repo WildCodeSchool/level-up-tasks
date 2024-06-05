bd868dfedd3e9cb1a336b157afabf65dc588b4cc
SirineBETTAIEB <sirinebettaieb30@gmail.com>
2024-06-05 16:17:13 +0200

    modification items

--- src/app/model/item/item.ts
+++ src/app/model/item/item.ts
@@ -1,8 +1,11 @@
 export class Item {
-  constructor(
-    readonly id: number,
-    readonly name: string,
-    readonly price: number,
-    readonly picture: string
-  ) {}
+  
+    constructor(
+        readonly id: number,
+        readonly name: string,
+        readonly email: string,
+        readonly picture: string,
+        readonly iconeGit :string,
+        readonly iconeLink :string
+      ) {}
 }
--- src/app/service/items/items.service.ts
+++ src/app/service/items/items.service.ts
@@ -6,10 +6,10 @@ import { Item } from '../../model/item/item';
 })
 export class ItemsService {
   readonly items: Item[] = [
-    new Item(1, 'Salamèche', 100, 'slameche.webp'),
-    new Item(2, 'Rondoudou', 200, 'rdoudou.jpeg'),
-    new Item(3, 'Pikachu', 300, 'fatkachu.png'),
-    new Item(4, 'Ronflex', 400, 'rflex.webp'),
+    new Item(1, 'Sirine BETTAIEB', 'sirinebettaieb30@gmail.com', 'avatar.png','https://github.com/sirinebettaieb','https://www.linkedin.com/in/sirine-bettaieb-bb5562177/'),
+    new Item(2, 'Lucie DANIS', 'lucie.lindanis@gmail.com', 'avatar.png','https://github.com/LucieLDa','https://www.linkedin.com/in/lucie-danis-053475306/'),
+    new Item(3, 'Fatimata DRAME', 'dramefatim75@gmail.com', 'avatar.png','https://github.com/Fatimata-dev','https://www.linkedin.com/in/ousseynou-fatimata75014/'),
+    new Item(4, 'Salma MOUBARKI', 'salmamoubarki@gmail.com', 'avatar.png','https://github.com/MoubarkiS','https://www.linkedin.com/in/salma-moubarki-867087130/'),
   ];
   constructor() {}
 }
