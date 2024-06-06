216df2af229a8eecf4ff97442605a94173890826
Fatimata <dramefatim75@gmail.com>
2024-06-06 17:18:07 +0200

    implementing footer comonent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../../nav-bar/nav-bar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink,NavBarComponent],
+  imports: [RouterLink,NavBarComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
