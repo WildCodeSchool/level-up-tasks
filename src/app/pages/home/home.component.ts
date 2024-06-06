83d12fbd26dd0d4c800edb9ecb3041ecc71831c3
Fatimata <dramefatim75@gmail.com>
2024-06-06 18:04:33 +0200

    display of footer in app

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../../nav-bar/nav-bar.component';
-import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink,NavBarComponent,FooterComponent],
+  imports: [RouterLink,NavBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
