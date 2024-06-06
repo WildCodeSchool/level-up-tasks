32f768ccd8cb69c9a212a7fee22c9f2ec01b2c24
LuDanis <lucie.lindanis@gmail.com>
2024-06-06 11:34:09 +0200

    ADD TaskList component, Task Component and Task class

---
+++ src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Task } from '../../model/item/task';
+import { TaskComponent } from '../task/task.component';
+
+@Component({
+  selector: 'app-task-list',
+  standalone: true,
+  imports: [CommonModule, TaskComponent],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.scss'
+})
+export class TaskListComponent {
+  isActive = true;
+  height : string = '100%';
+  listHeight !: string;
+
+  //mock data for task list, will be empty once addTask is implemented
+  taskList : Task[] = [
+    new Task(1, 'Faire 30 minutes de yoga', new Date("2024-05-16"), false),
+    new Task(2, 'Réunion', new Date("2024-06-08"), false)
+  ];
+
+  changeState(): void { 
+    (this.height === '0') ? this.height = this.listHeight : this.height = '0';
+    this.isActive = !this.isActive;
+  }
+
+  @ViewChild('myIdentifier')
+  myIdentifier!: ElementRef;
+
+  ngAfterViewInit() {
+    this.listHeight = this.myIdentifier.nativeElement.offsetHeight + "px";
+  }
+}
