67bb3b9ec18ff5588a0f7900e16b9797e4ddce8a
Benja-san <beugnet.freelance@gmail.com>
2024-06-07 13:48:57 +0200

    merge changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-=======
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
->>>>>>> 51e8e4f21f1ab7d3677a6a22771f774e8dfdfbf7
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet],
-=======
-  imports: [RouterOutlet,NavBarComponent,FooterComponent],
->>>>>>> 51e8e4f21f1ab7d3677a6a22771f774e8dfdfbf7
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-17-project-template';
