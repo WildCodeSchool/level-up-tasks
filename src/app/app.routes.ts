0037bc0a6df7daf02434e4c033f1ac36e5002785
Benja-san <beugnet.freelance@gmail.com>
2024-06-07 13:51:38 +0200

    hotfix dev

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-<<<<<<< HEAD
-import { ItemsComponent } from './pages/items/items.component';
-=======
 
 import { TaskPageComponent } from './pages/task-page/task-page.component';
 import { TeamsComponent } from './pages/teams/teams.component';
->>>>>>> 51e8e4f21f1ab7d3677a6a22771f774e8dfdfbf7
 
 export const routes: Routes = [
   {
@@ -18,11 +14,6 @@ export const routes: Routes = [
     redirectTo: '/home',
     pathMatch: 'full',
   },
-<<<<<<< HEAD
-  {
-    path: 'items',
-    component: ItemsComponent,
-=======
 
   {
     path: 'teams',
@@ -31,6 +22,5 @@ export const routes: Routes = [
   {
     path: 'tasks',
     component: TaskPageComponent,
->>>>>>> 51e8e4f21f1ab7d3677a6a22771f774e8dfdfbf7
   },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
-=======
-import { Component, HostListener } from '@angular/core';
->>>>>>> 51e8e4f21f1ab7d3677a6a22771f774e8dfdfbf7
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -12,10 +8,4 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-<<<<<<< HEAD
 export class HomeComponent {}
-=======
-export class HomeComponent {
-
-}
->>>>>>> 51e8e4f21f1ab7d3677a6a22771f774e8dfdfbf7
