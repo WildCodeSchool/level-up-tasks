188b2a1787cd70ce38c3775ede531213d9ae884a
Fatimata <dramefatim75@gmail.com>
2024-06-13 10:53:38 +0200

    some fixes

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -23,9 +23,6 @@ import { NgIf } from '@angular/common';
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
-  //Use the ViewChild directive to access the fileInput DOM element referenced in the HTML template.
-  //ElementRef allows you to directly manipulate this element(fileInput).
-  //@ViewChild('fileInput', { static: false }) fileInput!: ElementRef;
   isModalOpen: boolean = false;
   submitted: boolean = false;
   teams: UserTeams[] = [
@@ -53,12 +50,6 @@ export class ProfileComponent {
   toggleModale() {
     this.isModalOpen = !this.isModalOpen;
   }
-
-  //this methode declanch an clic on fileInput which opens the file selection dialog box.
-  // triggerFileInput() {
-  //   this.fileInput.nativeElement.click();
-  // }
-
   //This method is called when the user selects a file. It retrieves the selected file and stores it in a variable.
   //createObjectURL() creates a DOMString containing a URL representing the selected image src
   onFileSelected(event: any) {
