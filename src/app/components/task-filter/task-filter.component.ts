b63e5f6e9635b927803238be2eba5fe8167e0265
LuDanis <lucie.lindanis@gmail.com>
2024-06-21 11:46:25 +0200

    ADD task-progression to mobile

--- src/app/components/task-filter/task-filter.component.ts
+++ src/app/components/task-filter/task-filter.component.ts
@@ -29,21 +29,21 @@ export class TaskFilterComponent {
 
   
   onFilterDescriptions():void{
-    //this.filteredDate='';
-    //this.selectedlevel= "";
+    this.filteredDate='';
+    this.selectedlevel= "";
     this.filterDescriptions.emit(this.filterValue);
   }
 
   onFilterDates():void{
-    //this.filterValue='';
-    //this.selectedlevel= "";
+    this.filterValue='';
+    this.selectedlevel= "";
     this.filterDate.emit(this.filteredDate);
   }
 
 
   onFilterImportance():void{
-    //this.filterValue='';
-    //this.filteredDate='';
+    this.filterValue='';
+    this.filteredDate='';
     this.filterImportancy.emit(this.selectedlevel);
   }
 }
