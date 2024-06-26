0233d3924baf6dcbafeaf372c076c7cce09d5a24
Fatimata <dramefatim75@gmail.com>
2024-06-26 17:11:12 +0200

    fix review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule} from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 import { AppComponent } from './app.component';
