c1e35c5632675b51be96f111b8c7af991595f3e0
Fatimata <dramefatim75@gmail.com>
2024-07-10 17:43:14 +0200

    blocking access to the registration and connection pages when the user is already logged in

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,24 @@
 
-import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { SignupComponent } from '../signup/signup.component';
 import { LoginComponent } from '../login/login.component';
+import { AuthenticationService } from '../../service/User/authentication.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
 
-  imports: [RouterLink, SignupComponent, LoginComponent],
+  imports: [RouterLink, SignupComponent, LoginComponent,CommonModule],
 
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 
 export class HomeComponent {
+  authService = inject(AuthenticationService);
 
 }
 
